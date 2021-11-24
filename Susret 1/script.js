71dd28405d7c1d6a305b72bad90fe9b6dcbc4579
pirusic.aida <pirusic.aida@nsoft.com>
2021-11-24 15:20:27 +0100

    Implement "vjezbe"

---
+++ Susret 1/script.js
@@ -0,0 +1,51 @@
+// 1. zadatak
+
+/*let a = 7;
+let b = 3;
+let zbir = a + b;
+let razlika = a - b;
+let kolicnik = a / b;
+let proizvod = a * b;
+
+console.log('Zbir je ' + zbir);
+console.log('Razlika je ' + razlika);
+console.log('Kolicnik je ' + kolicnik);
+console.log('Proizvod je ' + proizvod);*/
+
+// 2. zadatak
+/*let ime = 'Niko';
+let prezime = 'Nikic';
+
+console.log('Ime i prezime korisnika je ' + ime + ' ' + prezime);*/
+
+// 3. zadatak
+/* const car = {
+    marka: 'Volkswagen',
+    model: 'Golf VII',
+    registracijskaOznaka: 'AAA-0-BBB',
+    godinaProizvodnje: 2018,
+    boja: 'siva'
+}
+
+console.log('Model: ' + car.model);
+console.log('Marka: ' + car.marka);
+console.log('Registracijska oznaka: ' + car.registracijskaOznaka);
+console.log('Godina proizvodnje: ' + car.godinaProizvodnje);
+console.log('Boja: ' + car.boja);
+*/
+
+
+// 4. zadatak
+/*let imePrezime = prompt('Kako se zovete?');
+console.log('Ime i prezime korisnika je ' + imePrezime);*/
+
+// 5. zadatak
+/*let a = prompt('Unesite prvi broj');
+let b = prompt('Unesite drugi broj');
+let razlika = a - b;
+let kolicnik = a / b;
+let proizvod = a * b;
+
+alert('Razlika je ' + razlika);
+alert('Kolicnik je ' + kolicnik);
+alert('Proizvod je ' + proizvod);*/
