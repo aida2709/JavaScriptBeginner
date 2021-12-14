aff3731fabdef0e5cd24c7822013a3d1b312b89d
pirusic.aida <pirusic.aida@nsoft.com>
2021-12-14 12:15:36 +0100

    do - while

--- Petlje/script.js
+++ Petlje/script.js
@@ -25,4 +25,28 @@
 
 
 
+// 3.
+
+// const n = parseInt(prompt('Unesite n'));
+// const k = parseInt(prompt('Unesite k'));
+
+// let brojac = n;
+
+// console.log(`Parni brojevi u intervalu od ${n} do ${k} su: `);
+// do {
+//     if (brojac % 2 == 0) {
+//         console.log(brojac);
+//     }
+//     brojac++;
+// } while (brojac <= k);
+
+
+// 4. 
+
+// let brojac = 1;
+
+// do {
+//     console.log('DOBRODOSLI');
+//     brojac++;
+// } while (brojac <= 10);
 
