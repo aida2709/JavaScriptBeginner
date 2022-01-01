8b301c1cff6dd7f905e19d379d27bf03c68a8387
pirusic.aida <pirusic.aida@nsoft.com>
2022-01-01 14:28:41 +0100

    DOM zadatak 1

---
+++ DOM/rjesenja/zadatak1/script.js
@@ -0,0 +1,9 @@
+const fName = document.getElementsByName('fname');
+const lName = document.getElementsByName('lname');
+const form = document.getElementById('form1');
+
+const paragraph = document.createElement('p');
+paragraph.innerText = `Dobrodosli ${fName[0].getAttribute('value')} ${lName[0].getAttribute('value')}`;
+paragraph.className = 'welcome_p';
+
+form.appendChild(paragraph);
