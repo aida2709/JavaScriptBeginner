20c43492a5508e8ee53bc2e3878338cd7b16cad3
pirusic.aida <pirusic.aida@nsoft.com>
2022-01-07 13:24:52 +0100

    Change tekst zadatka

--- ToDo Organizer/script.js
+++ ToDo Organizer/script.js
@@ -1,25 +1,47 @@
-const students = [
+const tasks = [
     {
         id: 1,
-        firstName: 'Test',
-        lastName: 'Test',
-        gender: 'male',
-        years: 22,
-        city: 'Mostar'
+        title: 'Test',
+        content: 'Test',
+        finished: false
     },
     {
         id: 2,
-        firstName: 'John',
-        lastName: 'Doe',
-        gender: 'male',
-        years: 20,
+        title: 'Test2',
+        content: 'Test2',
+        finished: true
     },
-    {
-        id: 3,
-        firstName: 'Niko',
-        lastName: 'Nikic',
-        gender: 'female',
-        years: 20,
-        city: 'Mostar'
-    }
 ];
+
+
+const todoList = document.getElementById('todoList');
+const doneList = document.getElementById('doneList');
+
+function appendTaskToList(task) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    console.log('uslo', task);
+
+    card.innerHTML = `
+  <div class="card-body">
+    <h5 class="card-title">${task.title}</h5>
+    <p class="card-text">${task.content}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+    `;
+
+    if (task.finished) {
+        doneList.appendChild(card);
+    } else {
+        todoList.appendChild(card);
+    }
+}
+
+function renderTasks() {
+    tasks.forEach(task => {
+        appendTaskToList(task);
+    });
+}
+
+renderTasks();
