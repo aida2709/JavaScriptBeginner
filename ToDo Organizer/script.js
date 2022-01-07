266277288fd1b018c13f4f1bb3d6c17da3f15a09
pirusic.aida <pirusic.aida@nsoft.com>
2022-01-07 13:37:38 +0100

    render tasks into list

--- ToDo Organizer/script.js
+++ ToDo Organizer/script.js
@@ -11,29 +11,47 @@ const tasks = [
         content: 'Test2',
         finished: true
     },
+    {
+        id: 3,
+        title: 'Test3',
+        content: 'Test3',
+        finished: false
+    },
 ];
 
 
 const todoList = document.getElementById('todoList');
 const doneList = document.getElementById('doneList');
 
+function finishTask(taskId) {
+    console.log('zavrsi', taskId);
+}
+
 function appendTaskToList(task) {
+    // check https://getbootstrap.com/docs/5.0/components/card/
     const card = document.createElement('div');
     card.classList.add('card');
-
-    console.log('uslo', task);
 
     card.innerHTML = `
   <div class="card-body">
     <h5 class="card-title">${task.title}</h5>
     <p class="card-text">${task.content}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
     `;
 
     if (task.finished) {
         doneList.appendChild(card);
     } else {
+        const cardBody = card.querySelector('.card-body');
+        const zavrsiButton = document.createElement('a');
+        zavrsiButton.innerText = 'Zavrsi';
+        zavrsiButton.setAttribute('href', '#');
+        zavrsiButton.classList.add('btn', 'btn-primary');
+        zavrsiButton.onclick = function () {
+            finishTask(task.id);
+        }
+        cardBody.appendChild(zavrsiButton);
+
         todoList.appendChild(card);
     }
 }
