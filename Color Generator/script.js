18d9aedb4c9590aa2d8b9f9a9ae9991ff1757606
pirusic.aida <pirusic.aida@nsoft.com>
2022-01-09 17:20:51 +0100

    RGB generator

--- Color Generator/script.js
+++ Color Generator/script.js
@@ -2,10 +2,26 @@ const setHexBtn = document.getElementById('setHexBtn');
 const hexColorInput = document.getElementById('hexColor');
 const coloredDiv = document.getElementById('coloredDiv');
 const hexColorError = document.getElementById('hexColorError');
+const redInput = document.getElementById('red');
+const greenInput = document.getElementById('green');
+const blueInput = document.getElementById('blue');
+const setRGBBtn = document.getElementById('setRGBBtn');
+const rgbError = document.getElementById('rgbError');
 
 
 function colorizeDivHex(color) {
     coloredDiv.style.backgroundColor = color[0] !== '#' ? `#${color}` : color;
+}
+
+function colorizeDivRgb(color) {
+    coloredDiv.style.backgroundColor = `rgb(${color.red},${color.green},${color.blue})`;
+
+}
+
+function checkAreColorsValid(red, blue, green) {
+    return red && !isNaN(red)
+        && blue && !isNaN(blue)
+        && green && !isNaN(green);
 }
 
 setHexBtn.onclick = function () {
@@ -17,4 +33,20 @@ setHexBtn.onclick = function () {
         hexColorError.classList.add('hide');
         colorizeDivHex(color);
     }
+}
+
+
+
+setRGBBtn.onclick = function () {
+    let red = redInput.value;
+    let blue = blueInput.value;
+    let green = greenInput.value;
+
+    if (!checkAreColorsValid(red, blue, green)) {
+        rgbError.classList.remove('hide');
+    } else {
+        rgbError.classList.add('hide');
+        colorizeDivRgb({ red, blue, green });
+    }
+
 }
