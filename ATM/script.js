7258ce148b530b13e4b133542417b70ad09c6c6e
pirusic.aida <pirusic.aida@nsoft.com>
2022-01-16 20:48:46 +0100

    Stanje racuna - ATM

--- ATM/script.js
+++ ATM/script.js
@@ -3,19 +3,22 @@ const users = [
         firstName: 'Niko',
         lastName: 'Nikic',
         cardNumber: '123456',
-        PIN: '0000'
+        PIN: '0000',
+        amount: '25000'
     },
     {
         firstName: 'Test',
         lastName: 'Test',
         cardNumber: '789123',
-        PIN: '1111'
+        PIN: '1111',
+        amount: '10000'
     },
     {
         firstName: 'Miro',
         lastName: 'Miric',
         cardNumber: '654321',
-        PIN: '2222'
+        PIN: '2222',
+        amount: '37000'
     }
 ];
 
@@ -28,6 +31,8 @@ const loginBtn = document.getElementById('loginBtn');
 const cardNumberError = document.getElementById('cardNumberError');
 const pinError = document.getElementById('pinError');
 const cancelBtn = document.getElementById('cancelBtn');
+const balanceBtn = document.getElementById('balanceBtn');
+const balanceDiv = document.getElementById('balance');
 
 function hideErrors() {
     cardNumberError.classList.add('hide'); pinError
@@ -54,11 +59,13 @@ function checkIsFormValid() {
 function showHomePage() {
     loginDiv.classList.add('hide');
     homeDiv.classList.remove('hide');
+    balanceDiv.classList.add('hide');
 }
 
 function showLoginPage() {
     loginDiv.classList.remove('hide');
     homeDiv.classList.add('hide');
+    balanceDiv.classList.add('hide');
 }
 
 function cleanUpForm() {
@@ -88,4 +95,10 @@ loginBtn.onclick = function () {
 cancelBtn.onclick = function () {
     currentUser = null;
     showLoginPage();
+}
+
+balanceBtn.onclick = function () {
+    balanceDiv.classList.remove('hide');
+    const balanceAmount = document.getElementById('balanceAmount');
+    balanceAmount.innerText = `Stanje racuna iznosi ${currentUser.amount} KM`;
 }
