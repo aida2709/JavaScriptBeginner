107b52d338ff5347d759182563350b799d8f4665
aida2709 <pirusic.aida@nsoft.com>
2022-02-15 20:03:53 +0100

    Ispit zadatak 3

--- Ispit/BookList/script.js
+++ Ispit/BookList/script.js
@@ -1,13 +1,11 @@
-- Kreirati h1 sa sadrzajem "My Book List".
-
-- Kopirati slijedeci niz knjiga:
-    const books = [
+const books = [
     {
         title: 'The Design of EveryDay Things',
         author: 'Don Norman',
         alreadyRead: false,
         url: 'https://www.amazon.com/Design-Everyday-Things-Revised-Expanded/dp/0465050654'
-    }, {
+    },
+    {
         title: 'The Most Human Human',
         author: 'Brian Christian',
         alreadyRead: true,
@@ -18,9 +16,23 @@
         author: 'Markus Zusak',
         alreadyRead: true,
         url: 'https://www.amazon.com/Book-Thief-Markus-Zusak/dp/0375842209/ref=sr_1_1?crid=1XG08069AOI5D&keywords=The+Book+Thief&qid=1641049848&s=books&sprefix=the+book+thief%2Cstripbooks-intl-ship%2C216&sr=1-1'
+    },
+    {
+        title: 'The Stranger',
+        author: 'Albert Camus',
+        alreadyRead: true,
+        url: 'https://www.goodreads.com/book/show/49552.The_Stranger'
+    },
+    {
+        title: 'A Tale of Two Cities',
+        author: 'Charles Dickens',
+        alreadyRead: false,
+        url: 'https://www.goodreads.com/book/show/1953.A_Tale_of_Two_Cities'
+    },
+    {
+        title: 'The Little Prince',
+        author: 'Antoine de Saint-Exupéry',
+        alreadyRead: true,
+        url: 'https://www.goodreads.com/book/show/157993.The_Little_Prince'
     }
 ];
-
-- Kreirati listu ciji ce item-i biti naziv knjige + autor
-- Svaki list item treba da bude link koji vodi do url-a iz niza
-- Ako je knjiga procitana, pozadina itema treba biti zute boje
