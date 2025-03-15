6418e16b24d7d32260cd89018c8bdebc0ca1d589
Aida Pirusic <pirusic.aida@nsoft.com>
2025-03-15 17:25:45 +0100

    Translate Objects lection to english

---
+++ Loops/script.js
@@ -0,0 +1,128 @@
+// 1.
+
+// const n = Number(prompt('Enter n'));
+// const k = Number(prompt('Enter k'));
+
+// let counter = n;
+
+// console.log(`Even numbers in the range from ${n} to ${k} are: `);
+// while (counter <= k) {
+//     if (counter % 2 == 0) {
+//         console.log(counter);
+//     }
+//     counter++;
+// }
+
+
+// 2. 
+
+// let counter = 1;
+
+// while (counter <= 10) {
+//     console.log('WELCOME');
+//     counter++;
+// }
+
+
+// 3.
+
+// const n = Number(prompt('Enter n'));
+// const k = Number(prompt('Enter k'));
+
+// let counter = n;
+
+// console.log(`Even numbers in the range from ${n} to ${k} are: `);
+// do {
+//     if (counter % 2 == 0) {
+//         console.log(counter);
+//     }
+//     counter++;
+// } while (counter <= k);
+
+
+// 4. 
+
+// let counter = 1;
+
+// do {
+//     console.log('WELCOME');
+//     counter++;
+// } while (counter <= 10);
+
+
+// 5. 
+// a) while loop
+
+// const n = Number(prompt('Enter n'));
+// const k = Number(prompt('Enter k'));
+
+// let counter = n;
+// let sum = 0;
+// let evenSum = 0;
+// let product = 1;
+// let totalNumbers = 0;
+
+// while (counter <= k) {
+//     if (counter % 2 == 0) {
+//         evenSum += counter;
+//     } else {
+//         product *= counter;
+//     }
+
+//     sum += counter;
+//     totalNumbers++;
+//     counter++;
+// }
+
+// console.log(`The sum of even numbers in the range from ${n} to ${k} is: ${evenSum}`);
+// console.log(`The product of odd numbers in the range from ${n} to ${k} is: ${product}`);
+// console.log(`The arithmetic mean of numbers in the range from ${n} to ${k} is: ${sum / totalNumbers}`);
+
+
+// b) do-while loop
+
+// const n = Number(prompt('Enter n'));
+// const k = Number(prompt('Enter k'));
+
+// let counter = n;
+// let sum = 0;
+// let evenSum = 0;
+// let product = 1;
+// let totalNumbers = 0;
+
+// do {
+//     if (counter % 2 == 0) {
+//         evenSum += counter;
+//     } else {
+//         product *= counter;
+//     }
+
+//     sum += counter;
+//     totalNumbers++;
+//     counter++;
+// } while (counter <= k);
+
+// console.log(`The sum of even numbers in the range from ${n} to ${k} is: ${evenSum}`);
+// console.log(`The product of odd numbers in the range from ${n} to ${k} is: ${product}`);
+// console.log(`The arithmetic mean of numbers in the range from ${n} to ${k} is: ${sum / totalNumbers}`);
+
+
+
+// 6.
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// 7.
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+
+// 8.
+// let n = Number(prompt('Enter n: '));
+// for(let i = 1; i <= 10; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
